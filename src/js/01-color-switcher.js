7713210413b7f0f92a64896efe7e43d29686e894
VolontyrPavel <pavel.volontyr@gmail.com>
2023-05-12 23:34:45 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+refs = {
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+}
 
+refs.start.addEventListener('click', onStart);
+refs.stop.addEventListener('click', onStop);
+
+function onStart() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStop() {
+    console.log(777);
+}
+
+
+
+
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
