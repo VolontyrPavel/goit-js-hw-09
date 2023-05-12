19cd75bd0b8d93b45e0d8fcf109cb146d3fb3d8e
VolontyrPavel <103372792+VolontyrPavel@users.noreply.github.com>
2023-05-12 20:35:13 +0000

    Deploying to gh-pages from @ VolontyrPavel/goit-js-hw-09@7713210413b7f0f92a64896efe7e43d29686e894 🚀

---
+++ 01-color-switcher.21e65f43.js
@@ -0,0 +1,2 @@
+refs={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.querySelector("body")},refs.start.addEventListener("click",(function(){refs.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`})),refs.stop.addEventListener("click",(function(){console.log(777)}));
+//# sourceMappingURL=01-color-switcher.21e65f43.js.map
---
+++ 01-color-switcher.bd066174.js
@@ -0,0 +1,2 @@
+refs={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.querySelector("body")},refs.start.addEventListener("click",(function(){refs.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))})),refs.stop.addEventListener("click",(function(){console.log(777)}));
+//# sourceMappingURL=01-color-switcher.bd066174.js.map
