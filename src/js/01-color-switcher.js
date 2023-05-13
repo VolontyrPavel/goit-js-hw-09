7a65972c8d3afc3130369edc20f3685e5a7e1957
VolontyrPavel <pavel.volontyr@gmail.com>
2023-05-13 11:28:01 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,23 +3,28 @@ refs = {
     stop: document.querySelector('[data-stop]'),
     body: document.querySelector('body'),
 }
+let timerId = null;
 
-refs.start.addEventListener('click', onStart);
+refs.start.addEventListener('click', onStartInterval);
 refs.stop.addEventListener('click', onStop);
+
+refs.stop.setAttribute('disabled', true);
+
+function onStartInterval() {
+    refs.start.setAttribute('disabled', true);
+    refs.stop.removeAttribute('disabled');
+    timerId = setInterval(onStart, 1000)
+}
 
 function onStart() {
     refs.body.style.backgroundColor = getRandomHexColor();
 }
 
 function onStop() {
-    console.log(777);
+    clearInterval(timerId);
+    refs.start.removeAttribute('disabled');
+    refs.stop.setAttribute('disabled', true);
 }
-
-
-
-
-
-
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
